75342a20207324911961e2808be64998f698e963
Viki2705 <fauzifiki9@gmail.com>
2024-05-31 14:21:18 +0700

    Add project files.

---
+++ Tugas_Polymorpishm/Epson.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_Polymorpishm
+{
+    public class Epson : PrinterWindows
+    {
+        public override void Show()
+        {
+            Console.WriteLine("Epson display dimension: 10*12");
+        }
+        public override void Print()
+        {
+            Console.WriteLine("Canon printer printing...");
+        }
+    }
+}
